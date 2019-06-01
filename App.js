7eb5a04df9144a0c375723095aec6128216267a3
Emre Akurek <emreyeprem@gmail.com>
2019-05-31 19:30:31 -0500

    components setup

--- App.js
+++ App.js
@@ -1,43 +1,52 @@
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, TextInput, Button} from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
+import ListItem from './src/components/ListItem/ListItem';
 
 export default class App extends Component {
-   state= {
-     placeName: '',
-     places: []
-   }
+  state = {
+    placeName: "",
+    places: []
+  };
 
- placeNameChangedHandler = val => {
-     this.setState({
-         placeName: val
-       });
+  placeNameChangedHandler = val => {
+    this.setState({
+      placeName: val
+    });
+  };
+
+  placeSubmitHandler = () => {
+    if (this.state.placeName.trim() === '') {
+      return;
     };
 
-placeSubmitHandler = () => {
-  if (this.state.placeName.trim() === "") {
-    return;
-  }
-
-  this.setState(prevState => {
-    return {
-       places: prevState.places.concat(prevState.placeName)
-     };
-   });
-};
+    this.setState(prevState => {
+      return {
+        places: prevState.places.concat(prevState.placeName)
+      };
+    });
+  };
 
   render() {
+    const placesOutput = this.state.places.map((place, i) => (
+      <ListItem key={i} placeName={place} />
+    ));
     return (
       <View style={styles.container}>
         <View style={styles.inputContainer}>
-           <TextInput
-             style={styles.placeInput}
-             placeholder= "An awesome place"
-             value={this.state.placeName} onChangeText={this.placeNameChangedHandler} />
-          <Button style={styles.placeButton}
-             onPress={this.placeSubmitHandler}
-             title="Add" />
+          <TextInput
+            placeholder="An awesome place"
+            value={this.state.placeName}
+            onChangeText={this.placeNameChangedHandler}
+            style={styles.placeInput}
+          />
+          <Button
+            title="Add"
+            style={styles.placeButton}
+            onPress={this.placeSubmitHandler}
+          />
         </View>
+        <View style={styles.listContainer}>{placesOutput}</View>
       </View>
     );
   }
@@ -46,10 +55,10 @@ placeSubmitHandler = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 45,
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    backgroundColor: '#fff',
+    padding: 46,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-start"
   },
   inputContainer: {
     // flex: 1,
@@ -63,5 +72,8 @@ const styles = StyleSheet.create({
   },
   placeButton: {
     width: "30%"
+  },
+  listContainer: {
+    width: "100%"
   }
 });
